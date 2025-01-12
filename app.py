4f96c07cd011f97217536ca63dc4a912e0a9532e
caliber123-dot <niyazpathan@gmail.com>
2025-01-13 02:11:40 +0530

    added channel name

--- app.py
+++ app.py
@@ -5,34 +5,58 @@
 # pip install gunicorn
 # pip install waitress
 
-from flask import Flask, render_template, request, redirect, jsonify
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from waitress import serve
-# import requests
+import pytz
 
 app = Flask(__name__)
+
+# import secrets
+# print("your_secret_key=",secrets.token_hex(16))  # Generates a 32-character hex string
+# app.secret_key = os.environ.get('SECRET_KEY', 'default_secret_key')
+app.secret_key = "db65336f04edb23664a676f8dc3a0e17"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///disease.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-app.app_context().push()
 
 class reg_tbl(db.Model):
     rg_id = db.Column(db.Integer, primary_key=True)
     rg_name = db.Column(db.String(100), nullable=False) # Cannot be NULL
     rg_email = db.Column(db.String(100), nullable=False)
     rg_phone = db.Column(db.String(50), nullable=False)
-    rg_psw = db.Column(db.String(50), nullable=False)
+    rg_psw = db.Column(db.String(200), nullable=False)
     rg_date = db.Column(db.DateTime, default=datetime.utcnow)
     rg_status = db.Column(db.Integer, nullable=False)
 
-    def __repr__(self) -> str:
-        return f"{self.rg_id} - {self.rg_name}"
-    
-# @app.route('/')
-# def start():
-#     return '<h1>This is Home Page 1</h1>'
+class predict_tbl(db.Model):
+    pr_id = db.Column(db.Integer, primary_key=True)
+    pr_date = db.Column(db.DateTime, default=datetime.utcnow)
+    pr_symptoms = db.Column(db.String(200), nullable=False) # Cannot be NULL
+    pr_disease = db.Column(db.String(150), nullable=False)
+    pr_status = db.Column(db.Integer, nullable=False)
+    # pr_rg_id = db.Column(db.Integer, nullable=False)
+    pr_rg_id = db.Column(db.Integer, db.ForeignKey('reg_tbl.rg_id'), nullable=False)  # Foreign Key
+
+    # def __repr__(self) -> str:
+    #     return f"{self.rg_id} - {self.rg_name}"  # Default representation
+    # def get_id(self) -> str:
+    #     return f"{self.rg_id}"  # Only ID
+    # def get_name(self) -> str:
+    #     return f"{self.rg_name}"  # Only Name
+    # def get_psw(self) -> str:
+    #     return f"{self.rg_psw}"  # Only Password
+
+# app.app_context().push()
+# Initialize the database
+with app.app_context():
+    db.create_all()
+
+india_timezone = pytz.timezone('Asia/Kolkata')
+# print(datetime.now(india_timezone))
 
 @app.route('/')
 def index():
@@ -40,24 +64,89 @@ def index():
     # print(all_data)
    return render_template('index.html')
 
-@app.route('/sign_in')
-def sign_in():
-   return render_template('sign_in.html')
+@app.route('/history')
+def history():
+    if 'user' in session:
+        flash(session['user'], 'success')
+        # allTodo = predict_tbl.query.all()
+        # Query all records where pr_rg_id = 1
+        allTodo = predict_tbl.query.filter_by(pr_rg_id=session['user_id']).all()
+        return render_template('history.html', allTodo=allTodo)
+    else:
+        return render_template('history.html')
 
-@app.route('/sign_up', methods=['GET', 'POST'])
+@app.route('/sign_in', methods=['GET', 'POST'])  # Login Page
+def sign_in():
+    if request.method == 'POST':
+        txtemail = request.form['txtemail']
+        txtpsw = request.form['txtpsw']
+        # existing_record = reg_tbl.query.filter(
+        #     (reg_tbl.rg_email == txtemail) & (reg_tbl.rg_psw == txtpsw)
+        # ).first()
+        user = reg_tbl.query.filter_by(rg_email=txtemail).first()
+        # print("GET VALUE :: ", user.rg_name)
+        # if user and check_password_hash(user.get_psw(), txtpsw):
+        if user and check_password_hash(user.rg_psw, txtpsw):
+        # if existing_record:
+            session['user_id'] = user.rg_id
+            session['user'] = user.rg_name
+            flash('Login successful! Hello, ' + session['user'], 'success')
+            return redirect(url_for('success'))
+        else:
+            flash('Invalid credentials. Please try again.', 'error')
+    elif 'user' in session:
+        flash('Hello, ' + session['user'] , 'success')
+        # return render_template('sign_in.html', username=session['user'])
+    return render_template('sign_in.html')  
+
+@app.route('/success')
+def success():
+    if 'user' in session:
+        return render_template('predict.html', username=session['user'])
+    else:
+        flash('Your session has expired.', 'warning')
+    return redirect(url_for('sign_in'))
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    flash('Logged out successfully.', 'info')
+    return redirect(url_for('sign_in'))
+
+@app.route('/sign_up', methods=['GET', 'POST']) # Register Page
 def sign_up():
-   if request.method=='POST':
-       txtname = request.form.get('txtname')
-       txtemail = request.form.get('txtemail')
-       txtphone = request.form.get('txtphone')
-       txtpsw = request.form.get('txtpsw')
-       txtcpsw = request.form.get('txtcpsw')
-       tosave = reg_tbl(rg_name=txtname, rg_email=txtemail, rg_phone = txtphone, rg_psw = txtpsw, rg_status = 1) # Add data
-       db.session.add(tosave)
-       db.session.commit()
-       myMsg = "Record saved successfully."
-       return render_template('sign_up.html', msg = myMsg)
-   return render_template('sign_up.html')
+    if request.method=='POST':
+        txtname = request.form.get('txtname')
+        txtemail = request.form.get('txtemail')
+        txtphone = request.form.get('txtphone')
+        txtpsw = request.form.get('txtpsw')
+        txtcpsw = request.form.get('txtcpsw')
+        # Check if email or phone number already exists
+        existing_record = reg_tbl.query.filter(
+            (reg_tbl.rg_email == txtemail) | (reg_tbl.rg_phone == txtphone)
+        ).first()
+        if existing_record:
+            myMsg = "Record already exists with this email or phone number."
+            # flash('Record already exists with this email or phone number.', 'error')
+            return render_template('sign_up.html', msg=myMsg, txtname=txtname, txtemail=txtemail, txtphone=txtphone)
+        else:
+            # Add new record
+            hashed_password = generate_password_hash(txtpsw)
+            tosave = reg_tbl(
+                rg_name=txtname,
+                rg_email=txtemail,
+                rg_phone=txtphone,
+                rg_psw=hashed_password,
+                rg_date=datetime.now(india_timezone),
+                rg_status=1
+            )
+            db.session.add(tosave)
+            db.session.commit()
+            flash('Registration successful! Please log in.', 'success')
+            return redirect(url_for('sign_in'))
+            # myMsg = "Registration successful! Please log in."
+            # return render_template('sign_in.html', msg = myMsg)
+    return render_template('sign_up.html')
 
 from model_optimize import predictDisease
 @app.route('/predict', methods=['GET', 'POST'])
@@ -100,9 +189,31 @@ def predict():
             "svm_prediction": svm_prediction,
             "final_prediction": final_prediction,
         }
+        # Define the timezone
+        
+        tosave = predict_tbl(
+                pr_date=datetime.now(india_timezone),
+                pr_symptoms=symptoms_string,
+                pr_disease=final_prediction,
+                pr_status=1,
+                pr_rg_id= session['user_id'] # Replace with a valid rg_id from reg_tbl
+            )
+        db.session.add(tosave)
+        db.session.commit()
+        flash('Record added successfully to ' + session['user'], 'success')
+        # flash('Hello, ' + session['user'] , 'success')
         # Pass the dictionary to the template
-        return render_template('predict.html',**symptoms,**prediction)
-    return render_template('predict.html')
+        return render_template('predict.html', **symptoms, **prediction, username=session['user'])
+        # if 'user' in session:
+        #     return render_template('predict.html', **symptoms,**prediction , username=session['user'])
+        # else:
+        #     return redirect(url_for('sign_in'))
+    # else:
+    elif 'user' in session:
+        # flash('You are already logged in!', 'success')
+        flash('Hello, ' + session['user'] , 'success')
+    # return render_template('predict.html')
+    return redirect(url_for('success'))
 
 if __name__ == "__main__":
     # db.create_all() 
