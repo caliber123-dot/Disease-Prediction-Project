05c7565d35dc486ff37ce5906bad1a10345e7657
caliber123-dot <niyazpathan@gmail.com>
2025-06-26 11:47:11 +0530

    first commit

--- mail.py
+++ mail.py
@@ -1,6 +1,10 @@
 # Ref. https://mailtrap.io/blog/python-send-email-gmail/
 import smtplib
 from email.mime.text import MIMEText
+sender = "caliberai123@gmail.com" #From
+recipients = ["deshmukharpit772@gmail.com"] # To
+# load_dotenv() # Load variables from .env file
+password = "ucoz pkvc cumx moak"
 
 def send_email(subject, body, sender, recipients, password):
     msg = MIMEText(body, 'html')
@@ -11,6 +15,6 @@ def send_email(subject, body, sender, recipients, password):
        smtp_server.login(sender, password)
        smtp_server.sendmail(sender, recipients, msg.as_string())
     return 1
-    # print("Email sent successfully!")
+    print("Email sent successfully!")
 
-# send_email(subject, body, sender, recipients, password)
+send_email("subject 1", "Hi", sender, recipients, password)
